1ee7727e0b374150e5ffb11f3e6004e25ec03c8e
riddhimaBhanja <22053182@kiit.ac.in>
2025-11-13 15:25:36 +0530

    Added functional programming version (Streams/Lambda)

--- src/CountIndiaFunctional.java
+++ src/CountIndiaFunctional.java
@@ -5,11 +5,10 @@ import java.util.stream.*;
 
 public class CountIndiaFunctional {
     public static void main(String[] args) {
-        // ✅ Change this to your actual file path
         String filePath = "C:\\Users\\KIIT\\Downloads\\chubb_workspace\\CountIndiaProject\\data.txt";
 
         try {
-            // Read all lines, split into words, filter 'India', and count
+           
             long count = Files.lines(Paths.get(filePath))
                     .flatMap(line -> Arrays.stream(line.split("\\s+")))
                     .filter(word -> word.equalsIgnoreCase("India"))
